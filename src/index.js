e9c52e1ed59e9a1462131c901de0a2ab47247ce5
03mirosima <sezenmiray2009@hotmail.com>
2022-10-08 00:26:00 +0300

    added redux and redux toolkit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import { render } from "react-dom";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./store/store";
 import App from "./components/App";
 import "./styles/index.scss";
 
-render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
