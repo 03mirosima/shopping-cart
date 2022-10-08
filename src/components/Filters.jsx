e86a8b5424620a26d26dff8538c6269e211c3229
03mirosima <sezenmiray2009@hotmail.com>
2022-10-09 02:01:15 +0300

    created component files

---
+++ src/components/Filters.jsx
@@ -0,0 +1,15 @@
+const Filters = () => {
+  const SortingComponent = () => {
+    return <div>sorting</div>;
+  };
+  const FilteringComponent = () => {
+    return <div className="">aa</div>;
+  };
+  return (
+    <section>
+      <SortingComponent />
+      <FilteringComponent />
+    </section>
+  );
+};
+export default Filters;
